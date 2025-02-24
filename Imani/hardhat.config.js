d11213a77cfaf5850a4eaedf17fd284d99f16ac9
Wayne Griffin <g2mediagraphics@gmail.com>
2025-02-24 16:25:17 -0500

    Update hardhat.config.js

--- Imani/hardhat.config.js
+++ Imani/hardhat.config.js
@@ -1,12 +1,27 @@
-require("@nomiclabs/hardhat-ethers");
-
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomicfoundation/hardhat-ethers"); 
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.20",
   networks: {
-    hardhat: {},
-    goerli: {
-      url: "https://goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-      accounts: [`0x${YOUR_PRIVATE_KEY}`],
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [], 
     },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+    polygon: {
+      url: "https://polygon-rpc.com/", 
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today", 
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY, 
   },
 };
