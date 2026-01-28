942957e7a66f906e0d3dfa083d10b6b2b1b23540
Wayne Griffin <g2mediagraphics@gmail.com>
2026-01-28 18:15:24 -0500

    Update Staking.js

--- Frontend/src/components/Dashboard/Staking.js
+++ Frontend/src/components/Dashboard/Staking.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { ethers } from 'ethers';
 import { useWallet } from '../context/WalletContext';
-import { getContractInstance } from '../getContractInstance';
+import { getContractInstance } from '../../getContractInstance';
 import { FaCheckCircle, FaExclamationTriangle } from 'react-icons/fa';
 import stakingBanner from '../assets/images/staking-banner.png';
 
