3f40d13bb4b31af145c7f613e226e335326ea9bb
Wayne Griffin <g2mediagraphics@gmail.com>
2026-01-28 18:28:54 -0500

    Update Lending.js

--- Frontend/src/components/Dashboard/Lending.js
+++ Frontend/src/components/Dashboard/Lending.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import { ethers } from "ethers";
 import { useWallet } from "../../context/WalletContext";
 import { getContractInstance } from "../../getContractInstance";
-import lendingGuideImage from "../assets/images/lending-guide-visual.png";
+import lendingGuideImage from "../../assets/images/lending-guide-visual.png";
 import { FaEthereum, FaBitcoin, FaDollarSign } from "react-icons/fa";
 
 const tokenAddresses = {
