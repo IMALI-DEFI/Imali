01575585296c3746decaa23734a1877879b636d1
Wayne Griffin <g2mediagraphics@gmail.com>
2026-01-30 00:19:08 -0500

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -56,7 +56,6 @@ export default function App() {
     <WalletProvider>
       {/* App shell */}
       <div className="min-h-[100svh] w-full overflow-x-hidden bg-gray-950 text-slate-100">
-    <div className="fixed inset-0 bg-red-500 z-[9999]" />
         <Header />
 
         {/* Main content */}
