215df302e3078641bbd48f1833665bce2804bc2b
Wayne Griffin <g2mediagraphics@gmail.com>
2026-02-10 21:52:40 -0500

    Refactor ProtectedRoute to remove loading state

--- Frontend/src/components/routing/ProtectedRoute.jsx
+++ Frontend/src/components/routing/ProtectedRoute.jsx
@@ -1,29 +1,12 @@
+// src/components/routing/ProtectedRoute.jsx
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
 import BotAPI from "../../utils/BotAPI";
 
 export default function ProtectedRoute() {
   const location = useLocation();
-  const [status, setStatus] = useState("loading");
 
-  useEffect(() => {
-    BotAPI.me()
-      .then(() => setStatus("ok"))
-      .catch(() => setStatus("fail"));
-  }, []);
-
-  if (status === "loading") {
-    return <div className="p-8 text-center">Checking session…</div>;
-  }
-
-  if (status === "fail") {
-    return (
-      <Navigate
-        to="/login"
-        replace
-        state={{ from: location.pathname }}
-      />
-    );
+  if (!BotAPI.isLoggedIn()) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
   }
 
   return <Outlet />;
