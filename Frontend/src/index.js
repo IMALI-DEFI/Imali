f40bde75c20b040a14bc541cd219fa1c5d99c217
Wayne Griffin <g2mediagraphics@gmail.com>
2026-04-08 13:56:58 -0400

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,10 +1,8 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { WalletProvider } from "./context/WalletContext";
-import { AuthProvider } from "./context/AuthContext";  // ✅ ADD THIS
 
 // ✅ THIS IS REQUIRED FOR TAILWIND
 import "./index.css";
@@ -12,11 +10,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>           {/* ✅ ADD THIS - MUST BE OUTSIDE WalletProvider */}
-        <WalletProvider>
-          <App />
-        </WalletProvider>
-      </AuthProvider>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
