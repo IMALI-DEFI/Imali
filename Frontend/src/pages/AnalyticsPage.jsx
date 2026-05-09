69259aec6edb626590abb1d24884d1b334d8e84b
Wayne Griffin <g2mediagraphics@gmail.com>
2026-05-09 11:51:03 -0400

    Add files via upload

---
+++ Frontend/src/pages/AnalyticsPage.jsx
@@ -0,0 +1,9 @@
+// src/pages/AnalyticsPage.jsx
+import React from 'react';
+import EnterpriseAnalytics from '../components/enterprise/analytics/EnterpriseAnalytics';
+
+const AnalyticsPage = () => {
+  return <EnterpriseAnalytics />;
+};
+
+export default AnalyticsPage;
---
+++ Frontend/src/pages/StrategiesPage.jsx
@@ -0,0 +1,9 @@
+// src/pages/StrategiesPage.jsx
+import React from 'react';
+import StrategiesLibrary from '../components/enterprise/strategies/StrategiesLibrary';
+
+const StrategiesPage = () => {
+  return <StrategiesLibrary />;
+};
+
+export default StrategiesPage;
