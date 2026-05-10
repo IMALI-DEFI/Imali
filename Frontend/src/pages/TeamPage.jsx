874c5fd8aef0d8d5248efa6ff92999de7481e550
Wayne Griffin <g2mediagraphics@gmail.com>
2026-05-09 20:18:50 -0400

    Add files via upload

---
+++ Frontend/src/pages/TeamPage.jsx
@@ -0,0 +1,6 @@
+// src/pages/TeamPage.jsx
+import React from 'react';
+import TeamManagement from '../components/enterprise/team/TeamManagement';
+
+const TeamPage = () => <TeamManagement />;
+export default TeamPage;
