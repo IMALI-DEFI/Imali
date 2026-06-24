760fc8bac240f41aa798fa5d84709ec1f21d98ef
Wayne Griffin <g2mediagraphics@gmail.com>
2026-06-24 08:32:04 -0400

    Create LoadingSpinner.jsx

---
+++ Frontend/src/components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+// imali/Frontend/src/components/LoadingSpinner.jsx
+import React from 'react';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="flex items-center justify-center min-h-[200px]">
+      <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
